38706e0dcbe7ca0e20593c5c0b22df72da4bdc81
mnonne <aminadzhiev@gmail.com>
2018-01-22 19:06:45 +0200

    Works only with one descriptor

--- get_next_line.c
+++ get_next_line.c
@@ -1,11 +1,70 @@
 #include "get_next_line.h"
 
-int     get_next_line(const int fd, char **line)
+int		find_line(char **stack, char **line)
 {
-    static t_lst    *stack;
-    char            *heap;
+	char	*ptr;
+	int		st;
+	int		end;
+	int		dif;
 
-    if (!line || fd < 0 || !(heap = (char *)malloc(sizeof (char) * (BUFF_SIZE + 1))))
-        return (-1);
-    
+	st = 0;
+	ptr = *stack;
+	while (ptr[st] != '\n' && ptr[st] != '\0')
+		st++;
+	end = st;
+	while (ptr[end] != '\0')
+		end++;
+	if (**stack != '\0')
+	{
+		*line = ft_strnew(st + 1);
+		ft_memcpy(*line, *stack, st);
+		if ((dif = end - st) == 0)
+			ft_bzero(*stack, end);
+		ft_memmove(*stack, *stack + st + 1, dif);
+		while (dif++ < end)
+			ptr[dif] = '\0';
+		return (1);
+	}
+	return (0);
+}
+
+int		gnl_read(int fd, char **stack, char *heap, char **line)
+{
+	char	*tmp;
+	int		ret;
+	int		res;
+
+	ft_bzero(heap, BUFF_SIZE + 1);
+	while ((ret = read(fd, heap, BUFF_SIZE)) > 0)
+	{
+		if (*stack)
+		{
+			tmp = ft_strdup(*stack);
+			free(*stack);
+			*stack = ft_strjoin(tmp, heap);
+			free(tmp);
+		}
+		else
+			*stack = ft_strdup(heap);
+		ft_bzero(heap, BUFF_SIZE + 1);
+	}
+	if (ret == -1)
+		return (-1);
+	res = find_line(stack, line);
+	return (res);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char		*stack;
+	char			*heap;
+	int				ret;
+
+	if (fd < 0 || !line || !(heap = (char *)malloc(sizeof(char) *
+					BUFF_SIZE + 1)))
+		return (-1);
+	*line = NULL;
+	ret = gnl_read(fd, &stack, heap, line);
+	free(heap);
+	return (ret);
 }
